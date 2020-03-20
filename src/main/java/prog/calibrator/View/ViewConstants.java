c71fbb83d9949d808ecf9cee785ee9c25c65fc48
NickGol <43709923+NickGol@users.noreply.github.com>
2020-03-20 17:36:06 +0300

    First GUI Version

---
+++ src/main/java/prog/calibrator/View/ViewConstants.java
@@ -0,0 +1,26 @@
+package prog.calibrator.View;
+
+public class ViewConstants {
+
+    final static String POLYNOMIAL_CHART_TEXT = "Вид полинома";
+    final static String X_AXIS_POLYNOMIAL_CHART_TEXT = "Данные сигнала";
+    final static String Y_AXIS_POLYNOMIAL_CHART_TEXT = "Реальные значения";
+    final static String SIGNAL_CHART_TITLE = "Форма калибруемого сигнала";
+    final static String X_AXIS_SIGNAL_CHART_TEXT = "Время";
+    final static String Y_AXIS_SIGNAL_CHART_TEXT = "Амплитуда";
+    final static String ADD_CALIBRATION_BUTTON_TEXT = "Добавить";
+    final static String DELETE_CALIBRATION_BUTTON_TEXT = "Удалить";
+    final static String CHANGE_CALIBRATION_BUTTON_TEXT = "Изменить";
+    final static String CREATE_CONNECTION_BUTTON_TEXT = "Связь";
+    final static String RAW_DATA_CALIBRATION_SETTER_LABEL = "Сигнал";
+    final static String REAL_DATA_CALIBRATION_SETTER_LABEL = "Значение";
+    final static String ENABLE_POLYNOMIAL_TEXT = "Применить полином";
+    final static String POLYNOMIAL_FORMULA_INDICATOR_LABEL = "Формула полинома";
+    final static String SIGNAL_AMPLITUDE_INDICATOR_LABEL = "Амплитуда";
+    final static String SIGNAL_MAXIMUM_INDICATOR_LABEL = "Максимум";
+    final static String SIGNAL_MINIMUM_INDICATOR_LABEL = "Минимум";
+    final static String SIGNAL_AVERAGE_INDICATOR_LABEL = "Срднее";
+    final static String SIGNAL_RMS_INDICATOR_LABEL = "СКЗ";
+    final static int WINDOW_WIDTH = 1200;
+    final static int WINDOW_HEIGHT = 600;
+}
