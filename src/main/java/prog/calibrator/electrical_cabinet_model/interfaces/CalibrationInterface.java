3c1a9768d983226b95ec00a945202a6d2df297b3
Andrey Khrapkov <andrey.i.khrapkov@gmail.com>
2020-03-24 17:29:53 +0300

    Add calibration interface.

---
+++ src/main/java/prog/calibrator/electrical_cabinet_model/interfaces/CalibrationInterface.java
@@ -0,0 +1,22 @@
+package prog.calibrator.electrical_cabinet_model.interfaces;
+
+import prog.calibrator.electrical_cabinet_model.channels.ReceiveChannel;
+import prog.calibrator.electrical_cabinet_model.observer.ObserverElectricalCabinet;
+
+public interface CalibrationInterface {
+    ReceiveChannel[] getReceiveChannel();
+    ReceiveChannel getReceiveChannel(int channelNumber);
+    ReceiveChannel getReceiveChannel(String channelName);
+
+    boolean setReceiveChannel(ReceiveChannel[] receiveChannels);
+    boolean setReceiveChannel(ReceiveChannel receiveChannel);
+
+    Float[] getChannelData(String channelName);
+    Float[] getChannelData(int channelNumber);
+
+
+    void subscribe(ObserverElectricalCabinet observer);
+    void unsubscribe(ObserverElectricalCabinet observer);
+
+    Thread getThread();
+}
