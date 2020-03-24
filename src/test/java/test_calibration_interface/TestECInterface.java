b323dc7d03ee7b5ef3d829f50be16048d555d29d
Andrey Khrapkov <andrey.i.khrapkov@gmail.com>
2020-03-24 17:36:09 +0300

    Add manual test for the calibration interface.

---
+++ src/test/java/test_calibration_interface/TestECInterface.java
@@ -0,0 +1,59 @@
+package test_calibration_interface;
+import prog.calibrator.electrical_cabinet_model.interfaces.CalibrationInterface;
+import prog.calibrator.electrical_cabinet_model.observer.EventType;
+import prog.calibrator.electrical_cabinet_model.observer.ObserverElectricalCabinet;
+
+import java.util.Scanner;
+
+/*
+ * Manual test for:
+ * CalibrationInterface.java
+ * EventType.java
+ * Observable.java
+ * ObserverElectricalCabinet.java
+ */
+class TestECInterface implements ObserverElectricalCabinet {
+    static boolean THREADS_STATUS = true;
+    CalibrationInterface cabinet = new CabinetExample();
+
+    public static void main(String[] args) {
+
+        TestECInterface main = new TestECInterface();
+        main.run();
+    }
+
+    private void run(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("s - subscribe\nu - unsubscribe\ne - exit");
+        char command = 0;
+        while (command != 'e'){
+            command = scan.next().charAt(0);
+            if(command == 's'){
+                System.out.println("Subscribe");
+                this.cabinet.subscribe(this);
+            }
+            if (command == 'u'){
+                System.out.println("Unsubscribe");
+                this.cabinet.unsubscribe(this);
+            }
+        }
+
+        TestECInterface.THREADS_STATUS = false;
+        Thread t = cabinet.getThread();
+        try{
+            t.join();
+        }
+        catch(InterruptedException e){
+
+            System.out.printf("%s has been interrupted", t.getName());
+        }
+
+        System.out.println("Thread closed!");
+        scan.close();
+    }
+
+    @Override
+    public void notify(EventType e) {
+        System.out.println(this.cabinet.getChannelData(0)[0]);
+    }
+}
